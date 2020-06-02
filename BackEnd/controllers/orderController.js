622f8ab69422b0db5644df545ee0832859b9552d
SofianSaleh <sofiane.saleh11@egmail.com>
2020-06-02 15:42:12 +0200

    finishing the orders routes

--- BackEnd/controllers/orderController.js
+++ BackEnd/controllers/orderController.js
@@ -1,6 +1,7 @@
 const Order = require("../database/Order");
 const OrderDetails = require("../database/OrderDetails");
 
+// fetching all the orders
 let getAllOrders = async () => {
   try {
     const orders = await Order.find()
@@ -36,6 +37,85 @@ let getAllOrders = async () => {
     throw e;
   }
 };
+
+// fetching a single order
+let getSingleOrder = async (order_id) => {
+  try {
+    const singleOrder = await Order.findOne({ _id: order_id })
+      .populate({
+        path: "orders_id",
+        model: "OrderDetails",
+        populate: {
+          path: "product_id",
+          model: "Product",
+          select: "title image price",
+          populate: {
+            path: "category_id",
+            model: "Category",
+            select: "title",
+          },
+        },
+      })
+      .populate({
+        path: "user_id",
+        model: "User",
+        select: "username firstName lastName email",
+      })
+      .lean();
+
+    return {
+      count: 1,
+      message: singleOrder
+        ? ` All orders have been fetched `
+        : ` No orders were found `,
+      singleOrder,
+    };
+  } catch (e) {
+    throw e;
+  }
+};
+
+// placing new order
+let addProductsToOrder = async (products) => {
+  try {
+    let arrOfIds = [];
+    for (let i = 0; i < products.length; i++) {
+      let newOrderedProduct = new OrderDetails({
+        product_id: products[i].id,
+        quantity: products[i].quantity,
+      });
+      await newOrderedProduct.save();
+      arrOfIds.push(newOrderedProduct._id);
+    }
+    console.log(`arrofids`, arrOfIds);
+
+    return arrOfIds;
+  } catch (e) {
+    throw e;
+  }
+};
+// Creating the order in the database
+let createOrder = async ({ user_id, products }) => {
+  try {
+    const ordersArr = await addProductsToOrder(products);
+    let newOrder = new Order({
+      user_id,
+      orders_id: ordersArr,
+    });
+
+    await newOrder.save();
+
+    return {
+      message: `Your order was saved successfully`,
+      newOrder,
+    };
+  } catch (e) {
+    throw e;
+  }
+};
+
 module.exports = {
   getAllOrders,
+  getSingleOrder,
+  createOrder,
 };
--- BackEnd/server/routes/order.js
+++ BackEnd/server/routes/order.js
@@ -1,8 +1,13 @@
 const express = require(`express`);
-const { getAllOrders } = require("../../controllers/orderController");
+const {
+  getAllOrders,
+  getSingleOrder,
+  createOrder,
+} = require("../../controllers/orderController");
 
 const router = express.Router();
 
+// get all orders
 router.get(`/`, async (req, res) => {
   try {
     const orders = await getAllOrders();
@@ -12,6 +17,33 @@ router.get(`/`, async (req, res) => {
       count: 0,
       message: e.message,
       orders: null,
+    };
+  }
+});
+
+// get a single order
+router.get(`/:id`, async (req, res) => {
+  try {
+    const order = await getSingleOrder(req.params.id);
+
+    res.json(order);
+  } catch (e) {
+    return {
+      count: 0,
+      message: e.message,
+      order: null,
+    };
+  }
+});
+
+// add an order
+router.post(`/add`, async (req, res) => {
+  try {
+    const ordered = await createOrder(req.body);
+    res.json(ordered);
+  } catch (e) {
+    return {
+      message: e.message,
     };
   }
 });

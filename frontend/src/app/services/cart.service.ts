9eed80cc705733646dcfda0e3b25e726ea8476f8
SofianSaleh <sofiane.saleh11@egmail.com>
2020-06-04 10:27:48 +0200

    working on product fetching

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -3,15 +3,17 @@ import {HttpClient} from '@angular/common/http';
 import {ProductService} from './product.service';
 import {OrderService} from './order.service';
 import {environment} from '../../environments/environment';
-import {CartModelPublic, CartModelServer} from "../module/cart.module";
-import {ProductModelServer} from "../module/product.module";
+import {CartModelPublic, CartModelServer} from '../module/cart.module';
+import {BehaviorSubject} from 'rxjs';
+import { Router} from '@angular/router';
+import {ProductModelServer} from '../module/product.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  private serverUrl = environment.SERVICE_URL
+  private serverUrl = environment.SERVICE_URL;
 
   /* Variable to store data in the local storage*/
   private cartDataClient: CartModelPublic = {
@@ -20,21 +22,65 @@ export class CartService {
       inCart: 0,
       _id: 0
     }]
-}
+};
 
   /* Variable to store data in the server */
-  private cartDataClient: CartModelServer = {
+  private cartDataServer: CartModelServer = {
     total: 0,
     data: [{
       product: undefined,
       numberOfItemsInCart: 0
   }]
-  }
+  };
 
-
+  // Observables to subscribe
+  cartTotal$ = new BehaviorSubject<number>(0);
+  cartData$ = new BehaviorSubject<CartModelServer>(this.cartDataServer);
 
 
   constructor(private http: HttpClient,
               private productService: ProductService,
-              private orderService: OrderService) { }
+              private orderService: OrderService,
+              private router: Router) {
+
+    this.cartTotal$.next(this.cartDataServer.total);
+    this.cartData$.next(this.cartDataServer);
+    //  get the info from local Storage (if any)
+    const info: CartModelPublic = JSON.parse(localStorage.getItem('cart'));
+
+    if (info !== null && info !== undefined && info.prodData){
+      // Local storage is not empty
+      this.cartDataClient = info;
+      //  Loop through and put it in the cartDataServer
+      this.cartDataClient.prodData.forEach((p) => {
+
+        this.productService.getSingleProduct(p._id).subscribe((actualProduct: ProductModelServer) => {
+
+          if (this.cartDataServer.data[0].numberOfItemsInCart === 0){
+            this.cartDataServer.data[0].numberOfItemsInCart = p.inCart;
+            this.cartDataServer.data[0].product = actualProduct;
+          // Calculate the value
+            this.cartDataClient.total = this.cartDataServer.total;
+            localStorage.setItem('cart', JSON.stringify(this.cartDataClient));
+
+          }else {
+          //  that means there is something in the local storage
+            this.cartDataServer.data.push({
+              numberOfItemsInCart: p.inCart,
+              product: actualProduct
+            });
+            // Calculate the value
+            this.cartDataClient.total = this.cartDataServer.total;
+            localStorage.setItem('cart', JSON.stringify(this.cartDataClient));
+          }
+          this.cartData$.next({...this.cartDataServer});
+        });
+      });
+    }
+  }
+
+//  add items to the cart
+  addProductToCart(id: number, quantity?: number) {
+    
+  }
 }
